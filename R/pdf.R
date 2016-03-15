3fb035cb6e259b13a67e38ecab9af5245b3e0a4d
NattyE <nathan.eastwood@icloud.com>
2016-03-15 20:52:02 +0000

    Added additional manuals. Added the plots. Documented the PDF function.

--- R/pdf.R
+++ R/pdf.R
@@ -1,4 +1,15 @@
-tdPDF <- pdf <- function(x, S = 1, r, c, t) {
+#' Calculate the PDF
+#'
+#' Calculate values of the probability density function.
+#'
+#' @param x Points at which to calculate the the pdf.
+#' @param S Unknown
+#' @param r,c,t Parameter values within the model.
+#'
+#' @return A vector of values from the pdf.
+#'
+#' @export
+tdPDF <- function(x, S = 1, r, c, t) {
   ### Need to find out if S is needed in moments, etc.
   -S * ((1 - (r / (1 + exp(-c * (x - t))))) ^ x) *
     (log(1 - (r / (1 + exp(-c * (x - t))))) -
---
+++ R/plots.R
@@ -0,0 +1,71 @@
+augmentMultiple <- function(...) {
+  models <- list(...)
+  lapply(models,
+         function(.) {
+           augData <- broom::augment(.)
+           colnames(augData) <- c("x", "y", "fitted", "resid")
+           augData
+         })
+}
+
+#' Plot the timedist CDF
+#'
+#' Given a model (or models) of class \code{timedist}, produce a cumulative distribution plot for
+#' each of them.
+#'
+#' @param ... A model (or a list of models) of class \code{timedist}.
+#'
+#' @import ggplot2
+#' @export
+ggtimedistCDF <- function(...) {
+  models <- list(...)
+  if (length(models) == 1) {
+    data <- broom::augment(...)
+    colnames(data) <- c("x", "y", "fitted", "resid")
+    ggplot(data = data) +
+      geom_point(aes(x = "x", y = "y")) +
+      geom_line(aes(x = "x", y = "fitted"))
+  } else {
+    data <- augmentMultiple(...)
+    modelNo <- paste0("model", rep(seq_along(data), lapply(data, nrow)))
+    data <- do.call("rbind", data)
+    data$group <- modelNo
+    ggplot(data = data, aes(x = "x", y = "y", colour = "group")) +
+      geom_point() +
+      geom_line(aes(x = "x", y = "fitted", colour = "group"))
+  }
+}
+
+#' Plot the timedist PDF
+#'
+#' Given a model (or models) of class \code{timedist}, produce a probability density function plot
+#' for each of them.
+#'
+#' @param ... A model (or a list of models) of class \code{timedist}.
+#' @param xlim The x limits (x1, x2) of the plot.
+#'
+#' @import ggplot2
+#' @export
+ggtimedistPDF <- function(..., xlim = NULL) {
+  params <- lapply(list(...), function(.) .$m$getPars())
+  data <- augmentMultiple(...)
+  modelNo <- paste0("model", seq_along(data))
+  data <- do.call("rbind", data)
+  if (is.null(xlim)) xlim <- seq(min(data$x), max(data$x), by = max(data$x) / 1000)
+  pdfData <- lapply(params, function(.) {
+    data.frame(y = tdPDF(xlim, S = 1, .["r"], .["c"], .["t"]),
+               x = xlim)
+  })
+  nRows <- lapply(pdfData, nrow)
+  pdfData <- do.call("rbind", pdfData)
+  pdfData$group <- rep(modelNo, nRows)
+  models <- list(...)
+  p <- if (length(models) > 1) {
+    ggplot(data = pdfData, aes_string(x = "x", y = "y", colour = "group")) +
+      geom_line()
+  } else {
+    ggplot(data = pdfData, aes_string(x = "x", y = "y")) +
+      geom_line()
+  }
+  p
+}
