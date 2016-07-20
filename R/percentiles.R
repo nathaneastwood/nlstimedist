581f37f510ab4eaa813aa6dde0645c5d53e9edcc
NattyE <nathan.eastwood@icloud.com>
2016-07-20 21:19:09 +0100

    Added the tdPercentiles function and updated some documentation

--- R/percentiles.R
+++ R/percentiles.R
@@ -1,36 +1,34 @@
-# Percentiles
-# Need to define upper
-timedistPercentiles <- function(object, n, upper = object$m$getPars()["t"] * 10, ...) {
-  params <- object$m$getPars()
+#' Calculate percentiles
+#'
+#' Calculate the percentiles for a given model output
+#'
+#' @param model An object of class \code{timedist}.
+#' @param n A vector of percentiles to be calculated.
+#' @param upper The upper end point of the interval to search.
+#' @param ... Additional parameters to be passed to \code{\link[stats]{uniroot}}.
+#'
+#' @examples
+#' \dontrun{
+#' tdPercentiles(model, n = 0.5)
+#' tdPercentiles(model, n = seq(0, 0.9, 0.1))
+#' }
+#'
+#' @export
+tdPercentiles <- function(model, n, upper = model$m$getPars()["t"] * 10, ...) {
+  params <- model$m$getPars()
   percentile <- function(x, y, r, c, t) {
     1 - (1 - (r / (1 + exp(-c * (x - t))))) ^ x - y
   }
-  uniroot(percentile, lower = 0, upper = Inf, y = n, r = params["r"],
-          c = params["c"], t = params["t"], ...)$root
-}
-#franco_percentiles(lm1, n = 0.99)
-
-# Create a new class, then object will be of class "new class"
-nth_raw_moment_wrap <- function(object, n, ...) {
-
-  params <- object$m$getPars()
-
-  nth_raw_moment <- function(x, n, r, c, t) {
-    x ^ (n - 1) * (1 - (r / (1 + exp(-c * (x - t))))) ^ x
+  vals <- if (length(n) > 1) {
+    do.call("c",
+            lapply(n, function (x) {
+              uniroot(percentile, lower = 0, upper = upper, y = x, r = params["r"],
+                      c = params["c"], t = params["t"], ...)$root
+            }))
+  } else {
+    uniroot(percentile, lower = 0, upper = upper, y = n, r = params["r"],
+            c = params["c"], t = params["t"], ...)$root
   }
-  n * integrate(nth_raw_moment, lower = 0, upper = Inf, n = n, r = params["r"], c = params["c"], t = params["t"])$value
+  names(vals) <- paste0(n * 100, "%")
+  vals
 }
-#nth_raw_moment_wrap(lm1, 0.99)
-
-# Percentile
-nth_raw_moment_wrap_maple <- function(object, ...) {
-
-  params <- object$m$getPars()
-
-  nth_raw_moment <- function(x, r, c, t) {
-    1 - (1 - (r / (1 + exp(-c * (x - t))))) ^ x
-  }
-  integrate(nth_raw_moment, lower = 0, upper = Inf,
-            r = params["r"], c = params["c"], t = params["t"])$value
-}
-#nth_raw_moment_wrap_maple(lm1)
