a3089a067acd07bdc5a602828d3f850805cd01cd
NattyE <neastwood@mango-solutions.com>
2016-09-08 21:59:18 +0100

    Removed unnecessary comment

--- R/pdf.R
+++ R/pdf.R
@@ -10,7 +10,6 @@
 #'
 #' @export
 tdPDF <- function(x, S = 1, r, c, t) {
-  ### Need to find out if S is needed in moments, etc.
   -S * ((1 - (r / (1 + exp(-c * (x - t))))) ^ x) *
     (log(1 - (r / (1 + exp(-c * (x - t))))) -
        (x * r * c * exp(-c * (x - t))) /
