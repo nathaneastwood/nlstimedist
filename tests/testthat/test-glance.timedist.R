800e601c3b4572f3790ba3b45d9cfc9ccaa0c826
nathaneastwood <neastwood@mango-solutions.com>
2018-10-27 17:13:19 +0100

    fix: Correct test on Windows builds

--- tests/testthat/test-glance.timedist.R
+++ tests/testthat/test-glance.timedist.R
@@ -4,16 +4,15 @@ test_that("Ensure the glance method is returning expected values", {
   data   <- lobelia[lobelia$Temperature == 12.5, ]
   data   <- tdData(data, x = "Day", y = "Germination")
   model  <- timedist(data, x = "Day", y = "propMax", r = 0.1, c = 0.5, t = 10)
-  out    <- glance(model)
+  out    <- as.data.frame(glance(model))
   expect <- structure(
-    list(sigma = 0x1.e7d8cbce1133ap-5, isConv = TRUE, finTol = 0x1p-26,
-         logLik = 0x1.550d5a88be3dcp+4, AIC = -0x1.150d5a88be3dcp+5,
-         BIC = -0x1.009a322d5f4f2p+5, deviance = 0x1.3f92a128e2ae8p-5,
-         df.residual = 11L, RSS = 0x1.efb75762f359bp-1),
-    .Names = c("sigma", "isConv", "finTol", "logLik", "AIC", "BIC", "deviance",
-               "df.residual", "RSS"),
-    row.names = c(NA, -1L),
-    class = c("tbl_df", "tbl", "data.frame")
+    list(
+      sigma = 0.0595516186299733, isConv = TRUE, finTol = 1.49011611938477e-08,
+      logLik = 21.3157601682213, AIC = -34.6315203364427,
+      BIC = -32.0752910179816, deviance = 0.0390103480959477, df.residual = 11L,
+      RSS = 0.968195658520631
+    ),
+    class = "data.frame", row.names = c(NA, -1L)
   )
   expect_equal(out, expect)
 })
